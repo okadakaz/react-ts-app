954cec72fef6c489c283224b44b7c169080db549
Kazunori Okada <kazunooka@gmail.com>
2021-11-30 09:20:39 +0900

    add section76 files

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useCallback, useState } from "react"
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
     const history = useHistory();
+    const { showMessage } = useMessage();
 
     const [loading, setLoading] = useState(false);
 
@@ -14,11 +16,12 @@ export const useAuth = () => {
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then((res) => {
                 if (res.data) {
+                    showMessage({ title: "ログインしました", status: "info" })
                     history.push("/home")
                 } else {
-                    alert("ユーザが見つかりません");
+                    showMessage({ title: "ユーザーが見つかりません", status: "error" });
                 }
-            }).catch(() => alert("ログインできません")).finally((() => setLoading(false)));
-    }, []);
+            }).catch(() => showMessage({ title: "ログインできません", status: "error" })).finally((() => setLoading(false)));
+    }, [history, showMessage]);
     return { login, loading }
 }
